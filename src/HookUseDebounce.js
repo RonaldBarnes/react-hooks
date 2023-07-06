3f64220aa4e97a48b34fe18ad27afd042387cf72
Ronald Barnes <ron@ronaldbarnes.ca>
2023-07-06 02:23:05 -0700

    HookUseDebounce.js: * Added hook useTimeout.js source code to listing: it's included in this page.
    Signed-off-by: Ronald Barnes <ron@ronaldbarnes.ca>

--- src/HookUseDebounce.js
+++ src/HookUseDebounce.js
@@ -256,6 +256,8 @@ export default function HookUseDebounce()
 
 
 
+// useDebounce.js:
+// ///////////////
 
 import { useEffect } from "react";
 
@@ -274,6 +276,55 @@ export default function useDebounce(
 	useEffect(clear, []);
 	}
 
+
+
+
+
+// useTimeout.js:
+// //////////////
+
+import { useRef, useEffect, useCallback } from "react";
+
+export default function useTimeout(callback, delay)
+	{
+	// A new function is passed each render, useRef allows function persistance
+	const callbackRef = useRef(callback);
+	const timeoutRef = useRef();
+
+	// Initialize
+	useEffect( () => {
+		// Update our ref to new function each re-render:
+		callbackRef.current = callback;
+		}, [callback]);
+
+	// 
+	const set = useCallback( () => {
+		timeoutRef.current = setTimeout( () => callbackRef.current(), delay)
+		}, [delay]);
+
+	const clear = useCallback( () => {
+		timeoutRef.current && clearTimeout(timeoutRef.current);
+		}, []);
+
+	// At start-up, not each timer tick:
+	useEffect( () => {
+		set()
+		return clear
+		}, [delay, set, clear]
+		);
+
+	const restart = useCallback( () => {
+		clear();
+		set();
+		}, [clear, set]
+		);
+
+	// I spend HOURS trying to debug an issue where I used "reset" instead of "restart"
+	// to capture the return vars.
+	// Switching to array method to avoid in future:
+	// return { restart, clear }
+	return [ restart, clear ];
+	}	// end function useTimeout
 
 `;
 	const output = useSourceCode({code});
